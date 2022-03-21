1aa4488e3ed6c9defaf7d87784d67c42fefc73fd
Enrique Mendez <eqm@mit.edu>
2022-03-21 15:31:14 -0400

    First Commit.

---
+++ helloworld.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+__plugin_name__ = "Hello World"
+__plugin_version__ = "1.0.0"
+__plugin_description__ = "A quick 'Hello World' example Plugin for Octoprint"
+__plugin_pythoncompat__ = ">=2.7,<4"
